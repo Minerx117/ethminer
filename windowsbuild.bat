63f9066966fa683d0d87d4fbecd3b9a25c7f9dc9
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2021-06-18 13:03:19 +1200

    Update windowsbuild.bat
    cuda 11.3 windows build 3080ti and 3070ti support

--- windowsbuild.bat
+++ windowsbuild.bat
@@ -4,7 +4,7 @@ mkdir release
 mkdir build
 cd build
 
-setx CUDA_TOOLKIT_ROOT_DIR "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2"
+setx CUDA_TOOLKIT_ROOT_DIR "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.3"
 cmake .. -G "Visual Studio 16 2019" -DETHASHCL=ON -DAPICORE=ON -DETHASHCUDA=ON -T v142
 cmake --build . --config Release
 cd ..
